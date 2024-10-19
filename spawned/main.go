c78f0e03f1576c439e7cafbd1ee2eb48afded673
Teodor Janez Podobnik <48418580+dorkamotorka@users.noreply.github.com>
2024-10-19 11:03:36 +0200

    Update main.go

--- spawned/main.go
+++ spawned/main.go
@@ -17,7 +17,7 @@ import (
 func main() {
     // Remove resource limits for kernels <5.11.
     if err := rlimit.RemoveMemlock(); err != nil {
-	log.Fatal("Removing memlock:", err)
+		log.Fatal("Removing memlock:", err)
     }
 
     var ifname string
@@ -27,28 +27,28 @@ func main() {
     // Load the compiled eBPF ELF and load it into the kernel.
     var objs spawnedObjects
     if err := loadSpawnedObjects(&objs, nil); err != nil {
-	log.Fatal("Loading eBPF objects:", err)
+		log.Fatal("Loading eBPF objects:", err)
     }
     defer objs.Close()
 
     iface, err := net.InterfaceByName(ifname)
     if err != nil {
-	log.Fatalf("Getting interface %s: %s", ifname, err)
+		log.Fatalf("Getting interface %s: %s", ifname, err)
     }
 
     // Attach XDP program to the network interface.
     xdplink, err := link.AttachXDP(link.XDPOptions{
-	Program:   objs.XdpTcpCapture,
-	Interface: iface.Index,
+		Program:   objs.XdpTcpCapture,
+		Interface: iface.Index,
     })
     if err != nil {
-	log.Fatal("Attaching XDP:", err)
+		log.Fatal("Attaching XDP:", err)
     }
     defer xdplink.Close()
 
     rd, err := ringbuf.NewReader(objs.RingbufMap)
     if err != nil {
-	panic(err)
+		panic(err)
     }
     defer rd.Close()
 
@@ -59,29 +59,29 @@ func main() {
     maxCount := 1000
     var startTime time.Time
     for {
-	if count >= maxCount {
-	    break
+		if count >= maxCount {
+	    	break
         }
-	_, err := rd.Read()
-	if err != nil {
-	    if err == ringbuf.ErrClosed {
-		return
-	    }
-	    log.Printf("reading from ringbuf: %v", err)
-	    continue
-	}
-
-	if count == 0 {
+		_, err := rd.Read()
+		if err != nil {
+		    if err == ringbuf.ErrClosed {
+				return
+		    }
+		    log.Printf("reading from ringbuf: %v", err)
+		    continue
+		}
+	
+		if count == 0 {
             startTime = time.Now()
         }
 
-	wg.Add(1)
-	go func() {
-	    defer wg.Done()
-    	    time.Sleep(200 * time.Millisecond)
-	    log.Printf("Received bpf event into userspace...\n")
-	}()
-	count++
+		wg.Add(1)
+		go func() {
+		    defer wg.Done()
+	    	time.Sleep(200 * time.Millisecond)
+		    log.Printf("Received bpf event into userspace...\n")
+		}()
+		count++
     }
 
     // Wait for goroutines to finish
